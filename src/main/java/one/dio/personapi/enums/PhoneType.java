12facbd402bdd16ab3da9f538f3048883754ebe8
Ricardo <ricardomr8@hotmail.com>
2022-01-21 08:03:26 -0300

    5º
    Atualizado até a quinta parte do desafio

---
+++ src/main/java/one/dio/personapi/enums/PhoneType.java
@@ -0,0 +1,18 @@
+package one.dio.personapi.enums;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum PhoneType {
+
+    HOME("Home"),
+    MOBILE("Mobile"),
+    COMMERCIAL("Commercial");
+
+    private final String description;
+
+
+}
--- src/main/java/one/dio/personapi/mapper/PersonapiApplication.java
+++ src/main/java/one/dio/personapi/mapper/PersonapiApplication.java
@@ -1,4 +1,4 @@
-package one.dio.personapi;
+package one.dio.personapi.mapper;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
