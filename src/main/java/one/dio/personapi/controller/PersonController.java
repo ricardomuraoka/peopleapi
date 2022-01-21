2a6fff8c8ff5074019b5824248a9d816fdb57497
Ricardo <ricardomr8@hotmail.com>
2022-01-21 08:35:06 -0300

    atualização

--- src/main/java/one/dio/personapi/controller/PersonController.java
+++ src/main/java/one/dio/personapi/controller/PersonController.java
@@ -1,17 +1,29 @@
 package one.dio.personapi.controller;
 
+import one.dio.personapi.dto.MessageResponseDTO;
 import one.dio.personapi.entity.Person;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import one.dio.personapi.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
 @RequestMapping("/api/v1/people")
 public class PersonController {
 
-    @GetMapping
-    public String getBook() {
-        return "API Test!";
+    private PersonRepository personRepository;
+
+    @Autowired
+    public PersonController(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
+    @PostMapping
+    public MessageResponseDTO createPerson(@RequestBody Person person) {
+        Person savedPerson = personRepository.save(person);
+        return MessageResponseDTO
+                .builder()
+                .message("Created persin with id " + savedPerson.getId())
+                .build();
     }
 }
